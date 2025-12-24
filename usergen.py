0d2dbac8853fb6c5f77bcae5d8196c373f774eb4
Gabriel Getzie <gabe@kotsf.com>
2025-12-24 09:25:56 +0800

    move some scripts to uv

--- usergen.py
+++ usergen.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.11"
+# ///
 import random
+import argparse
 
 
 def get_users(n=50):
-    fn_path = "/Users/gabe/first_names_ascii.txt"
-    ln_path = "/Users/gabe/last_names_ascii.txt"
+    fn_path = "/Users/gabe/datasets/first_names_ascii.txt"
+    ln_path = "/Users/gabe/datasets/last_names_ascii.txt"
     with (
         open(fn_path, encoding="utf-8") as fn_file,
         open(ln_path, encoding="utf-8") as ln_file,
@@ -14,3 +19,18 @@ def get_users(n=50):
             {"name": " ".join([first, last]), "email": first[0] + last + "@example.com"}
             for first, last in zip(first_names, last_names)
         ]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate random user data")
+    parser.add_argument(
+        "-n",
+        "--number",
+        type=int,
+        default=50,
+        help="Number of users to generate (default: 50)",
+    )
+    args = parser.parse_args()
+    users = get_users(args.number)
+    for user in users:
+        print(user)
